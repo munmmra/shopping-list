922a3ea624889a4184c3835b5daa8a2c4be62e62
Munmmra <munmmra@gmail.com>
2023-09-29 13:35:11 -0600

    clear UI

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 const addItem = (event) =>{
     event.preventDefault();
@@ -19,6 +20,8 @@ const addItem = (event) =>{
     li.appendChild(button);
     itemList.appendChild(li);
     itemInput.value = '';
+
+    checkUI();
 };
 
 const createButton = (classes) => {
@@ -34,18 +37,34 @@ const createIcon = (classes) => {
 }
 const removeItem = (e) => {
     if(e.target.parentElement.classList.contains('remove-item')){
-        e.target.parentElement.parentElement.remove();
+        if(window.confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+            checkUI();
+        }
     }
 }
 const clearItems = (e) => {
     while(itemList.firstChild){
         itemList.firstChild.remove();
     }
+    checkUI();
 }
 
+const checkUI = () =>{
+    const items = itemList.querySelectorAll('li');
+    if(items.length === 0){
+        clearButton.style.display = 'none';
+        itemFilter.style.display = 'none';
+    }else{
+        clearButton.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
 
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 itemList.addEventListener('click', removeItem);
 clearButton.addEventListener('click', clearItems);
+
+checkUI();
